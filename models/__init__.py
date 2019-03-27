3bb15bf22b74b1b100b4779b5d6549f3465b2f13
ZacLima <zacarias.lima@intitecperu.com>
2019-03-27 18:10:38 -0500

    report valuated

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 from . import it_report_account_valuated
 from . import it_report_account_valuated_header
-from . import account_report_valuated
+#from . import account_report_valuated
